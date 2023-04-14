48cc18735b8ae637f64c7c52f6531e025e29176c
inacio88 <inacio88@outlook.com>
2023-04-14 10:36:25 -0300

    Tela Cad

---
+++ GerenciamentoClientes/Tela_Cad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciamentoClientes
+{
+    public partial class Tela_Cad : Form
+    {
+        public Tela_Cad()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- GerenciamentoClientes/Tela_Inicial.cs
+++ GerenciamentoClientes/Tela_Inicial.cs
@@ -21,6 +21,9 @@ namespace GerenciamentoClientes
         {
             dataGridView1 = new DataGridView();
             Lbl_Titulo = new Label();
+            Btn_Novo = new Button();
+            Btn_Editar = new Button();
+            Btn_Excluir = new Button();
             ((ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -44,9 +47,42 @@ namespace GerenciamentoClientes
             Lbl_Titulo.Text = "Lista de Clientes";
             Lbl_Titulo.Click += label1_Click;
             // 
+            // Btn_Novo
+            // 
+            Btn_Novo.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_Novo.Location = new Point(71, 452);
+            Btn_Novo.Name = "Btn_Novo";
+            Btn_Novo.Size = new Size(80, 39);
+            Btn_Novo.TabIndex = 2;
+            Btn_Novo.Text = "Novo";
+            Btn_Novo.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Editar
+            // 
+            Btn_Editar.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_Editar.Location = new Point(253, 452);
+            Btn_Editar.Name = "Btn_Editar";
+            Btn_Editar.Size = new Size(80, 39);
+            Btn_Editar.TabIndex = 3;
+            Btn_Editar.Text = "Editar";
+            Btn_Editar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Excluir
+            // 
+            Btn_Excluir.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_Excluir.Location = new Point(429, 452);
+            Btn_Excluir.Name = "Btn_Excluir";
+            Btn_Excluir.Size = new Size(80, 39);
+            Btn_Excluir.TabIndex = 4;
+            Btn_Excluir.Text = "Excluir";
+            Btn_Excluir.UseVisualStyleBackColor = true;
+            // 
             // Tela_Inicial
             // 
             ClientSize = new Size(736, 524);
+            Controls.Add(Btn_Excluir);
+            Controls.Add(Btn_Editar);
+            Controls.Add(Btn_Novo);
             Controls.Add(Lbl_Titulo);
             Controls.Add(dataGridView1);
             Name = "Tela_Inicial";
@@ -62,6 +98,9 @@ namespace GerenciamentoClientes
         }
 
         private DataGridView dataGridView1;
+        private Button Btn_Novo;
+        private Button Btn_Editar;
+        private Button Btn_Excluir;
         private Label Lbl_Titulo;
     }
 }
