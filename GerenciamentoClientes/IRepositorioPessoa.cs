f3c056991ddfdff840e279c4bae7fe63ed3454a6
inacio88 <inacio88@outlook.com>
2023-04-25 14:02:06 -0300

    inicio do irepositorio

---
+++ GerenciamentoClientes/IRepositorioPessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoClientes
+{
+    internal interface IRepositorioPessoa
+    {
+        public List<Pessoa> ObterTodos();
+        public void CriarPessoa();
+        public void Remover(int Id);
+        public Pessoa ObterPorId(int Id);
+
+        public Pessoa Atualizar(int Id);
+
+    }
+}
---
+++ GerenciamentoClientes/RepositorPessoa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoClientes
+{
+    internal class RepositorPessoa
+    {
+        public List<Pessoa> ObterTodos()
+        {
+            List<Pessoa> lista;
+
+            return lista;
+        }
+        public void CriarPessoa()
+        {
+
+        }
+        public void Remover(int Id)
+        {
+
+        }
+        public Pessoa ObterPorId(int Id)
+        {
+
+        }
+
+        public Pessoa Atualizar(int Id)
+        {
+
+        }
+    }
+}
