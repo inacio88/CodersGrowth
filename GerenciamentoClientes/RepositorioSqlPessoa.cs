72599ad466e6dc3c74fec72e4ed31edefcba1cc5
inacio88 <inacio88@outlook.com>
2023-04-28 13:59:56 -0300

    repositorsql pessoa

---
+++ GerenciamentoClientes/RepositorioSqlPessoa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoClientes
+{
+    internal class RepositorioSqlPessoa : IRepositorioPessoa
+    {
+        protected List<Pessoa> listaDePessoas = ListaPessoasSingleTon.obterInstancia();
+
+        public List<Pessoa> ObterTodasPessoas()
+        {
+            return listaDePessoas;
+        }
+
+        public void CriarPessoa(Pessoa pessoa)
+        {
+            listaDePessoas.Add(pessoa);
+        }
+        public void RemoverPessoa(int Id)
+        {
+            var pessoa = ObterPessoaPorId(Id);
+
+            listaDePessoas.Remove(pessoa);
+        }
+        public Pessoa ObterPessoaPorId(int Id)
+        {
+            var pessoaBuscada = listaDePessoas.Find(x => x.Id == Id);
+
+            return pessoaBuscada;
+        }
+
+        public Pessoa AtualizarPessoa(Pessoa pessoa)
+        {
+            var pessoaBuscada = ObterPessoaPorId(pessoa.Id);
+
+            pessoaBuscada = pessoa;
+
+            return pessoaBuscada;
+        }
+    }
+}
