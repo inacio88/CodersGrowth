d5e52b6ecfcb6515056c2150b88c200b1155db8b
inacio88 <inacio88@outlook.com>
2023-04-28 12:13:42 -0300

    alterando coluna

---
+++ GerenciamentoClientes/20230428_AlterarColunaPessoaTabela.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace GerenciamentoClientes
+{
+    [Migration(20230428113300)]
+    public class AlterarColunaPessoaTabela : Migration
+    {
+        public override void Down()
+        {
+            Alter.Column("DataNascimento")
+                .OnTable("Pessoa")
+                .AsString();
+        }
+        public override void Up()
+        {
+            Alter.Column("DataNascimento")
+                .OnTable("Pessoa")
+                .AsDateTime()
+                .NotNullable();
+        }
+        
+    }
+}
