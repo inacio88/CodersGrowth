c676fedf81a339fd5ac41ce632084c9e47d0151b
inacio88 <inacio88@outlook.com>
2023-06-02 10:53:25 -0300

    estrutura basica

--- WebAPI/wwwroot/controller/App.controller.js
+++ WebAPI/wwwroot/controller/App.controller.js
@@ -1,13 +1,11 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
+
 	return Controller.extend("sap.ui.gerenciamento.cliente.controller.App", {
-		onInit: function () {
-			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-		},
-		onShowHello: function () {
-			alert("Hello World");
-		}
+
 	});
+
 });
