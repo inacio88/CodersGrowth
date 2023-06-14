e00a01b4f5a24bdf8b574358413626b48431c2b5
inacio88 <inacio88@outlook.com>
2023-06-14 11:48:24 -0300

    criaçao finalizada

--- WebAPI/wwwroot/controller/FormCriar.controller.js
+++ WebAPI/wwwroot/controller/FormCriar.controller.js
@@ -2,8 +2,10 @@ sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
 	"sap/ui/core/routing/History",
+    "sap/m/MessageToast",
 
-], function(Controller, JSONModel, History) {
+
+], function(Controller, JSONModel, History, MessageToast) {
 "use strict";
 
 var PageController = Controller.extend("sap.ui.gerenciamento.cliente.controller.FormCriar", {
@@ -11,15 +13,48 @@ var PageController = Controller.extend("sap.ui.gerenciamento.cliente.controller.
     onInit: function (oEvent) {
         
         var oData = {
-            "nome": "naaaa",
-            "email": "mail",
-            "cpf": "000.000.000-32",
+            "nome": "",
+            "email": "",
+            "cpf": "",
             "dataNascimento": "",
         };
 
         var oModel = new JSONModel(oData);
         this.getView().setModel(oModel, "dadosFormularioCriar");
 
+    },
+
+    _enviarRequisicaoCriar: async function () {
+        var dadosFormularioCriar = this.getView().getModel("dadosFormularioCriar");
+        var data = dadosFormularioCriar.oData;
+
+        return fetch('/api/Cliente', {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .catch(err => console.log(err));
+    },
+    
+    aoClicarEmSalvar: async function () {
+        let resposta = this._enviarRequisicaoCriar()
+            .then(resp => resp.json())
+            .then(id => {
+                if(id == undefined){
+                    MessageToast.show("Erro ao cadastrar");
+                    return;
+                }
+
+                MessageToast.show("Cadastro realizado com sucesso!");
+                this._navegarParaDetalhes(id);
+            });
+    },
+    
+    _navegarParaDetalhes: function(id){
+        const rotaDetalhes = "detalhes";
+        var oRouter = this.getOwnerComponent().getRouter();
+        oRouter.navTo(rotaDetalhes, {clienteCaminho: window.encodeURIComponent(id)});
+        
     },
 
     aoclicarEmVoltar: function () {
