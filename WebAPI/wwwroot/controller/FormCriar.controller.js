cd4897b4ea6521185409c692a06836d2f8368792
inacio88 <inacio88@outlook.com>
2023-06-15 10:44:36 -0300

    validacao do nome completa

--- WebAPI/wwwroot/controller/FormCriar.controller.js
+++ WebAPI/wwwroot/controller/FormCriar.controller.js
@@ -37,17 +37,63 @@ var PageController = Controller.extend("sap.ui.gerenciamento.cliente.controller.
     },
     
     aoClicarEmSalvar: async function () {
-        let resposta = this._enviarRequisicaoCriar()
-            .then(resp => resp.json())
-            .then(id => {
-                if(id == undefined){
-                    MessageToast.show("Erro ao cadastrar");
-                    return;
-                }
+        
+        this._aoMudarCampoNome();
+        if (1 === 3) {
+            let resposta = this._enviarRequisicaoCriar()
+                .then(resp => resp.json())
+                .then(id => {
+                    if(id == undefined){
+                        MessageToast.show("Erro ao cadastrar");
+                        return;
+                    }
 
-                MessageToast.show("Cadastro realizado com sucesso!");
-                this._navegarParaDetalhes(id);
+                    MessageToast.show("Cadastro realizado com sucesso!");
+                    this._navegarParaDetalhes(id);
             });
+        }
+    },
+    
+    aoMudarCampo: function (oEvent) {
+		let oItem = oEvent.getSource();
+
+        if (oItem.getName() === "inputNome") {
+            let erros = this._validarNome(oItem.getValue());
+            this._addMensagensErro(oItem, erros);
+        }
+
+    },
+
+    _validarNome: function (nome) {
+        let erros = [];
+        let formatoNome = /^[a-zA-ZÀ-ÖØ-öø-ÿ ]*$/;
+        nome = nome.trim();
+        if (nome.length === 0) {
+            erros.push("Nome deve ter no mínimo 1 caractere");
+        }
+
+        console.log(nome.match(formatoNome));
+        if (!nome.match(formatoNome)){
+            erros.push("Não pode ter caracteres especiais ou números");
+        }
+
+        return erros;
+    },
+
+    _addMensagensErro: function (oItem, erros) {
+        if (erros.length > 0) {
+            let estadosErro = '';
+            oItem.setValueState("Error");
+            
+            erros.forEach(erro =>{
+                estadosErro = estadosErro +  "\n" + erro;
+            })
+
+            oItem.setValueStateText(estadosErro);
+        }
+        else {
+            oItem.setValueState("Success");
+        }
     },
     
     _navegarParaDetalhes: function(id){
